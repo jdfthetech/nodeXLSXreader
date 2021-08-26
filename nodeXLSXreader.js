a86b6a8162bb5cd59b71e2b687b8a541f4caa5ff
jdfthetech <jdf@onlinemarketspecialist.com>
2021-08-26 07:00:51 -0700

    refactored code to eliminate vars and also simplified a bit

--- nodeXLSXreader.js
+++ nodeXLSXreader.js
@@ -1,16 +1,19 @@
 
 // requires node-xlsx installed
 
-var xlsx = require('node-xlsx').default;
-
-let preheader = []
+let xlsx = require('node-xlsx').default;
 
 const workSheetsFromFile = xlsx.parse('/dir/filename');
 
 // build header
-for (var i in workSheetsFromFile){
-    for (var x in workSheetsFromFile[i]){
-        for (var l in workSheetsFromFile[i][x][0]){
+// i will be the sheets so this is designed with 1 sheet files
+// would want to specify the sheet manually if working with numerous sheets
+
+let preheader = []
+
+for (let i in workSheetsFromFile){
+    for (let x in workSheetsFromFile[i]){
+        for (let l in workSheetsFromFile[i][x][0]){
             preheader.push(workSheetsFromFile[i][x][0][l])
             
         }
@@ -23,9 +26,9 @@ let header = preheader.slice(1)
 // parse data to a row array
 let rows = []
 
-for (var i in workSheetsFromFile){
-    for (var x in workSheetsFromFile[i]){
-        for (var l in workSheetsFromFile[i][x]){
+for (let i in workSheetsFromFile){
+    for (let x in workSheetsFromFile[i]){
+        for (let l in workSheetsFromFile[i][x]){
             rows.push(workSheetsFromFile[i][x][l])
             
         }
@@ -36,14 +39,13 @@ for (var i in workSheetsFromFile){
 let obj = {}
 let finalArr = []
 
+for(let i in rows){
+    let keys = header
+    let values = rows[i]     
+    obj = Object.assign(...keys.map((k, n) => ({[k]: values[n]})))
+    finalArr.push(obj)
+ }
 
-   for(var i = 0; i < rows.length; i++){
-        let keys = header
-        let values = rows[i]     
-        obj = Object.assign(...keys.map((k, n) => ({[k]: values[n]})))
-        finalArr.push(obj)
-     }
-    
 
 console.log(finalArr)
 
